de1f6ddd558484c010772d99433ede8f425de623
ArlanBiati <arlangustavo@hotmail.com>
2020-06-04 10:57:55 -0300

    Iniciando o Back-end + Refatorando arquivos NJK

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+
+
+server.use(express.static('public'))
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+
+server.get('/', (req, res) => {
+    return res.render('index.njk')
+})
+
+server.get('/create-point', (req, res) => {
+    return res.render('create-point.njk')
+})
+
+server.get('/search', (req, res) => {
+    return res.render('search-results.njk')
+})
+
+
+
+server.listen(3000)
